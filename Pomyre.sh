52d6b84438e1317433737dcedb0ccf282ee4c8f9
Samual <75905433+samual-ux@users.noreply.github.com>
2020-12-15 10:34:49 +0530

    Update Pomyre.sh

--- Pomyre.sh
+++ Pomyre.sh
@@ -25,7 +25,7 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border anonymouskid
+toilet -f mono12 -F border samBRO
 echo -e "\e[4;34m This Bomber Was Created By samualjoseph \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: samualjoseph12@gmail.com \e[0m"
