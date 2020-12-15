ec06316e302ab841794e278a559c10d6628de182
Samual <75905433+samual-ux@users.noreply.github.com>
2020-12-15 10:25:11 +0530

    Update Pomyre.sh

--- Pomyre.sh
+++ Pomyre.sh
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 pip install urllib3 --user
 pip install requests --user
-echo This Script Was Made By anonymous kid>update.sam bro
+echo This Script Was Made By anonymous kid>update.sam
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -25,7 +25,7 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border anonymous kid (sam)
+toilet -f mono12 -F border anonymouskid
 echo -e "\e[4;34m This Bomber Was Created By samualjoseph \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: samualjoseph12@gmail.com \e[0m"
@@ -54,7 +54,7 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
+git clone https://github.com/TheanonymouskidTBomb
 if [[ -s TBomb/TBomb.sh ]];then
 cd TBomb
 cp -r -f * .. > temp
@@ -79,7 +79,7 @@ clear
 echo -e "\e[1;33m"
 figlet TBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border anonymous kid(sam)
+toilet -f mono12 -F border anonymouskid(sam)
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -89,9 +89,9 @@ echo "  [+] Protection List"
 echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  anonymous kid  \e[1;31m"
+echo -e "\e[1;33m      [*]  anonymouskid  \e[1;31m"
 echo "         [-] Mail At: ggspeedx29@gmail.com"
-echo -e "\e[1;33m      [*]  The Black hat   \e[1;31m"
+echo -e "\e[1;33m      [*]  The Black hat\e[1;31m"
 echo "         [-] Ping At: http://wa.me/917600140353"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
 echo "         [-] Ping At: https://t.me/Rieltar"
